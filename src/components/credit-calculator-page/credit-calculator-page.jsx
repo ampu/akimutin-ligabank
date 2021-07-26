685bd3563c46ff12e76303f83e1ec2a1b9e225c9
Andrey <ampu@yandex.ru>
2021-07-27 02:43:39 +0300

    Восстанавливает страницу

--- src/components/credit-calculator-page/credit-calculator-page.jsx
+++ src/components/credit-calculator-page/credit-calculator-page.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
 import {HeaderWithHeaderState as Header} from '../header/header';
-// import {HeroAreaWithSlider as HeroArea} from '../hero-area/hero-area';
-// import {ServicesWithSlider as Services} from '../services/services';
+import {HeroAreaWithSlider as HeroArea} from '../hero-area/hero-area';
+import {ServicesWithSlider as Services} from '../services/services';
 import {CreditCalculator} from './credit-calculator';
-// import {Offices} from '../offices/offices';
+import {Offices} from '../offices/offices';
 import {Footer} from '../footer/footer';
 
 const CreditCalculatorPage = () => {
   return <>
     <Header/>
     <main className="credit-calculator-page">
+      <HeroArea/>
+      <Services/>
       <CreditCalculator/>
+      <Offices/>
     </main>
     <Footer/>
   </>;
